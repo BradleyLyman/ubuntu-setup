77edfdb0a7ded8efefbf256c2eb4fe6b626b3fb8
Bradley Lyman <lyman.brad3211@gmail.com>
2017-04-23 15:31:43 -0700

    add xinitrc to install

--- install_xmonad.sh
+++ install_xmonad.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "install xmonad"
 sudo apt-get install xmonad
 
-cp -r ./xmonad/* ~/.xmonad
+echo "create xmonad dir"
+mkdir ~/.xmonad
+echo "copy xmonad config"
+cp ./xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 
 xmonad --recompile
+
+cp ./xmonad/xinitrc ~/.xinitrc
