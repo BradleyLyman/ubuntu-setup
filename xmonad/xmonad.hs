aef433c505a8417088249c34a1ddccb988df3a19
Bradley Lyman <lyman.brad3211@gmail.com>
2017-04-24 09:46:04 -0700

    remove window borders and add gap

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,15 @@
 import XMonad
+import XMonad.Layout.Spacing
 
 main = xmonad def
-    { terminal = "urxvt" 
+    { terminal = "urxvt"
+    , layoutHook = myLayoutHook
+    , focusFollowsMouse = myFocusFollowsMouse
+    , clickJustFocuses = myClickJustFocuses
+    , borderWidth = myBorderWidth
     }
+
+myBorderWidth = 0
+myFocusFollowsMouse = False
+myClickJustFocuses = True
+myLayoutHook = spacing 4 $ Tall 1 (3/100) (1/2)
