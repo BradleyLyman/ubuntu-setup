f43ed8b32e99ad2da98f0775de0dc3aa2cfbb79e
Bradley Lyman <lyman.brad3211@gmail.com>
2017-04-23 17:28:25 -0700

    setup xresources

--- install_xmonad.sh
+++ install_xmonad.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
 echo "install xmonad"
-sudo apt-get install xmonad
+sudo apt-get install xmonad ttf_dejavu
 
 echo "create xmonad dir"
 mkdir ~/.xmonad
 echo "copy xmonad config"
 cp ./xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 
+echo "recompile xmonad"
 xmonad --recompile
 
+echo "copy xinit"
 cp ./xmonad/xinitrc ~/.xinitrc
+
+echo "copy xresources"
+cp ./XResources/xresources ~/.Xresources
+
+echo "reload resources"
+xrdb ~/.Xresources
+
+echo "install urxvt"
+sudo apt-get install rxvt-unicode-256color ttf-dejavu
+
+echo "install resize-font extension"
+git clone https://github.com/simmel/urxvt-resize-font
+
+mkdir -p ~/.urxvt/ext
+cp urxvt-resize-font/resize-font ~/.urxvt/ext
+
+rm -rf urxvt-resize-font
+
