dbd41c2f1c94f2d3eff0c664a1d7d76a354f40dc
Bradley Lyman <lyman.brad3211@gmail.com>
2017-04-26 23:38:14 -0700

    update remove desktop script

--- remove_desktop.sh
+++ remove_desktop.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-echo "Modify System To Start In text Mode"
+echo " -> Modify System To Start In text Mode"
 sudo systemctl set-default multi-user.target
 
-echo "Uninstall ubuntu desktop environment"
+echo " -> Uninstall ubuntu desktop environment"
 sudo apt-get --purge remove ubuntu-desktop lightdm
 sudo rm -rf /var/lightdm-data /var/lightdm
 
-echo "Deleting old X-org crap"
+echo " -> Deleting old X-org crap"
 sudo rm -r /etc/X11/*
 
-sudo apt-get install xserver-xorg xserver-xorg-core xinit virtualbox-guest-x11 virtualbox-guest-utils
+echo " -> get xorg dependencies and other junk"
+sudo apt-get install xserver-xorg xserver-xorg-core xinit
 
 sudo chmod u+s /usr/lib/xorg/Xorg
 
+echo " -> reconfigure xserver"
 sudo dpkg-reconfigure xserver-xorg
+
+echo " -> setup keyboard"
+sudo dpkg-reconfigure keyboard-configuration
