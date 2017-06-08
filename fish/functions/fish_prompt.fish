e406b2ace6d78cdd54d7cf9f2f2f8664b835bf38
Bradley Lyman <brlyman@amazon.com>
2017-06-08 11:07:42 -0700

    update prompt to include git branch

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,13 @@
 function fish_prompt
     set -l yellow (set_color yellow)
+    set -l blue (set_color blue)
     set -l normal (set_color normal)
 
-    echo $yellow (prompt_pwd) ">> "
+    set branch (git branch 2>/dev/null | grep \* | sed 's/\*//g' | sed 's/ //g')
+
+    if set -q branch
+        echo $yellow(prompt_pwd) [$blue$branch$yellow] ">> "
+    else
+        echo $yellow (prompt_pwd) ">> "
+    end
 end
