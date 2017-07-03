bef4c4bcdd4bdee8ed5d17c3ec9ae5eb2b871674
Bradley Lyman <brlyman@amazon.com>
2017-07-03 13:32:02 -0700

    update install_ruby.rb to copy to mybin

--- install_ruby.rb
+++ install_ruby.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/ruby
 
-require './log.rb'
+require_relative './ruby/brlyman/log.rb'
 
 info "install ruby-dev"
-`sudo apt-get -y install ruby-dev`
+Process.wait Process.spawn "sudo apt-get -y install ruby-dev"
 
 info "install rb-inotify"
-`sudo gem install rb-inotify`
+Process.wait Process.spawn "sudo gem install rb-inotify"
+
+info "install my ruby scripts"
+Process.wait Process.spawn "mkdir -p ~/.config/mybin"
+Process.wait Process.spawn "cp -r ./ruby/* ~/.config/mybin"
