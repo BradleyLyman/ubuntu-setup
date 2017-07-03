6a9a5bc0ca91f8ff7819bd9ba2f73a93b2f05adf
Bradley Lyman <brlyman@amazon.com>
2017-07-03 15:43:57 -0700

    move urxvt extensions into urxvt subdir

--- XResources/install.rb
+++ XResources/install.rb
@@ -3,20 +3,6 @@
 require_relative '../ruby/brlyman/log.rb'
 
 def install
-    log_block "resize_font_ext" do
-        info "**BEGIN**"
-        info "clone ext"
-        system "git clone https://github.com/simmel/urxvt-resize-font"
-
-        info "copy ext"
-        system "mkdir -p ~/.urxvt/ext"
-        system "cp urxvt-resize-font/resize-font ~/.urxvt/ext"
-
-        info "remove temporary"
-        system "rm -rf urxvt-resize-font"
-        info "**DONE**"
-    end
-
     info "copy xresources"
     system "cp ./xresources ~/.Xresources"
 
@@ -25,9 +11,6 @@ def install
 end
 
 def uninstall
-    info "remove urxvt extensions"
-    system "rm -rf ~/.urxvt/ext"
-
     info "delete xresources"
     system "rm ~/.Xresources"
 end
---
+++ urxvt/install.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+require_relative '../ruby/brlyman/log.rb'
+
+def install
+    log_block "resize_font_ext" do
+        info "**BEGIN**"
+        info "clone ext"
+        system "git clone https://github.com/simmel/urxvt-resize-font"
+
+        info "copy ext"
+        system "mkdir -p ~/.urxvt/ext"
+        system "cp urxvt-resize-font/resize-font ~/.urxvt/ext"
+
+        info "remove temporary"
+        system "rm -rf urxvt-resize-font"
+        info "**DONE**"
+    end
+end
+
+def uninstall
+    info "remove urxvt extensions"
+    system "rm -rf ~/.urxvt/ext"
+end
+
+if ARGV.first == "-u"
+    uninstall
+else
+    install
+end
