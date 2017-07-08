2f356f540aa7c60194a97d04535839f44cca8d24
Bradley Lyman <brlyman@amazon.com>
2017-07-08 04:24:53 -0700

    Create context and tests

--- java/src/brlyman/results/Context.java
+++ java/src/brlyman/results/Context.java
@@ -1,5 +1,33 @@
 package brlyman.results;
 
-public class Context
+import java.util.*;
+
+/**
+ * This class represents a context layer from a test
+ * as defined by the HierarchicalContextRunner.
+ */
+public class Context implements Result
 {
+    public Context(final String name)
+    {
+        this.name = name;
+        this.results = new ArrayList<>();
+    }
+
+    @Override
+    public void display(Printer printer)
+    {
+        printer.block_indent(name, () ->
+        {
+            results.forEach((result) -> result.display(printer));
+        });
+    }
+
+    public void addResult(final Result result)
+    {
+        results.add(result);
+    }
+
+    private List<Result> results;
+    private final String name;
 }
