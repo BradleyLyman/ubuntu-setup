51e5f35a8ae7ce4ded2d6fc6cc87ade6d7661708
Bradley Lyman <brlyman@amazon.com>
2017-07-13 08:49:38 -0700

    context tests almost written, true is false

--- java/tst/brlyman/results/ContextTest.java
+++ java/tst/brlyman/results/ContextTest.java
@@ -6,24 +6,49 @@ import static org.hamcrest.Matchers.*;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
+import brlyman.results.fakeProcess.FakeProcess;
+import brlyman.results.fakeProcess.FakeProcess.Type;
 import de.bechte.junit.runners.context.HierarchicalContextRunner;
 
 @RunWith(HierarchicalContextRunner.class)
 public class ContextTest
 {
-    public class when_the_context_is_empty
+    public class given_a_named_context
     {
         @Test
-        public void then_the_output_should_be_just_the_name()
+        public void then_the_context_should_always_have_a_name()
         {
+            assertThat(context.name(), is(equalTo(CONTEXT_NAME)));
         }
 
-        private Context context = new Context(CONTEXT_NAME);
+        @Test
+        public void then_the_message_should_be_null()
+        {
+            assertThat(context.message(), is(nullValue()));
+        }
+
+        public class when_the_context_is_empty
+        {
+            @Test
+            public void then_a_process_should_not_handle_any_children()
+            {
+                FakeProcess proc = new FakeProcess();
+                context.apply(proc);
+                assertThat(proc.lastProcessed(), is(Type.Context));
+            }
+        }
+
+        public class when_the_context_has_chlidren
+        {
+            @Test
+            public void then_the_process_should_handle_each_child()
+            {
+                assertThat(true, is(false));
+            }
+        }
+
+        private Result context = new Context(CONTEXT_NAME);
     }
 
     static private final String CONTEXT_NAME = "context name";
-    static private final String SUB_CTX_NAME = "sub context name";
-    static private final String PASS = "passed test name";
-    static private final String FAIL = "failed test name";
-    static private final String FAIL_MSG = "failed test msg";
 }
