59c02d627cf839c176c89895b405e144677def8d
Bradley Lyman <brlyman@amazon.com>
2017-07-27 16:46:40 -0700

    add ./bin to front of classpath in runsingletest

--- java/runSingleTest.rb
+++ java/runSingleTest.rb
@@ -3,5 +3,5 @@
 require_relative './buildJavaProject.rb'
 
 puts "Running test... dont forget to set $CLASSPATH!"
-system "java -cp $CLASSPATH:turborunner.jar brlyman.TurboRunner " + ARGV.first
+system "java -cp ./bin:$CLASSPATH:turborunner.jar brlyman.TurboRunner " + ARGV.first
 
