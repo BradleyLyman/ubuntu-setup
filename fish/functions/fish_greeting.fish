59f67cecdfb031309e8df1ed818a454504017b62
Bradley Lyman <brlyman@amazon.com>
2017-11-08 14:22:30 -0800

    Update fish greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -3,14 +3,8 @@ function fish_greeting
     set -l highlight (set_color -o yellow)
 
     echo $color":"
-    echo $color":  One who speaks does not know"
-    echo $color":  One who knows does not speak"
-    echo $color":  "
-    echo $color":  Shut the mouth"
-    echo $color":  Close the gates"
-    echo $color":  Blunt the sharpness"
-    echo $color":  Loosen the knots"
-    echo $color":  Temper the glare"
-    echo $color":  Become one with the dust of the world"
+    echo $color":  when two opponents meet"
+    echo $color":     the one without an enemy"
+    echo $color":     will surely triumph"
     echo $color":"
 end
