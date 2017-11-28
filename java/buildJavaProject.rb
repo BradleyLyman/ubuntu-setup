70404fed1282dc37c8387c75ad07d464c3de65b6
Lyman <brlyman@amazon.com>
2017-11-28 14:52:33 -0800

    Copy resource files in buildjavaproject.rb

--- java/buildJavaProject.rb
+++ java/buildJavaProject.rb
@@ -5,3 +5,10 @@ puts ""
 system 'mkdir -p ./bin'
 system 'javac -cp `echo $CLASSPATH`:./bin -d bin `find . -name "*.java"`'
 
+# Copy resource files
+resource_output = `find ./src -name '*.resource'`
+resources = resource_output.split(' ')
+resources.each do |resource|
+    system "cp --parents #{resource} bin/"
+end
+
