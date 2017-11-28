6cf774313400338c835bf42de67cec1739315b37
Lyman <brlyman@amazon.com>
2017-11-28 15:04:15 -0800

    fix resource copying

--- java/buildJavaProject.rb
+++ java/buildJavaProject.rb
@@ -3,12 +3,14 @@
 puts "Building project... don't forget to export CLASSPATH!"
 puts ""
 system 'mkdir -p ./bin'
-system 'javac -cp `echo $CLASSPATH`:./bin -d bin `find . -name "*.java"`'
+#system 'javac -cp `echo $CLASSPATH`:./bin -d bin `find . -name "*.java"`'
 
 # Copy resource files
 resource_output = `find ./src -name '*.resource'`
 resources = resource_output.split(' ')
 resources.each do |resource|
-    system "cp --parents #{resource} bin/"
+    dest = resource.sub("src/", "bin/")
+    puts "#{resource} -> #{dest}"
+    system "cp #{resource} #{dest}"
 end
 
