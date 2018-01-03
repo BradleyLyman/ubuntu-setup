ef01f8ec95c4bf6df534fb88c795d7b3399d120f
Lyman <brlyman@amazon.com>
2018-01-03 15:10:33 -0800

    Add Rust libs to ldlibrary path

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,6 @@
 set -x PATH $PATH ~/.config/mybin
-set -x PATH $PATH $BRAZIL_CLI_BIN
+set -x PATH $PATH /apollo/env/SDETools/bin/
+set -x PATH $PATH ~/.cargo/bin/
+set -x PATH $PATH ~/.local/bin/
+
+set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH (rustc --print sysroot)/lib
