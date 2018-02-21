9e391b4e068b276b15ec7e16ddef470ca74e734d
Lyman <brlyman@amazon.com>
2018-02-21 14:00:00 -0800

    Update Fish Greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -3,8 +3,6 @@ function fish_greeting
     set -l highlight (set_color -o yellow)
 
     echo $color":"
-    echo $color":  when two opponents meet"
-    echo $color":     the one without an enemy"
-    echo $color":     will surely triumph"
+    echo $color":  the obstacle is the way"
     echo $color":"
 end
