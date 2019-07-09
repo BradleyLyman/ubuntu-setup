ae6cd8983b126d5a444d3e685e4b49d418e166a3
brlyman <brlyman@amazon.com>
2019-07-09 14:57:59 +0100

    Add Powershell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+Set-PSReadLineOption -EditMode "Vi" -Colors @{
+    Command = [ConsoleColor]::White
+    Comment = [ConsoleColor]::White
+    ContinuationPrompt = [ConsoleColor]::White
+    DefaultToken = [ConsoleColor]::White
+    Emphasis = [ConsoleColor]::White
+    Error = [ConsoleColor]::White
+    Keyword = [ConsoleColor]::White
+    Member = [ConsoleColor]::White
+    Number = [ConsoleColor]::White
+    Operator = [ConsoleColor]::White
+    Parameter = [ConsoleColor]::White
+    Selection = [ConsoleColor]::White
+    String = [ConsoleColor]::White
+    Type = [ConsoleColor]::White
+    Variable = [ConsoleColor]::White
+}
