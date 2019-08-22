6dc765e17060327c1efb50dfd21d3db598b7b802
brlyman <brlyman@amazon.com>
2019-08-22 16:08:19 +0100

    Install Ruby Language Server

--- ruby/install.rb
+++ ruby/install.rb
@@ -12,12 +12,22 @@ def install
     info "install my ruby scripts"
     system "cp -r ./* ~/.config/mybin"
     system "rm ~/.config/mybin/install.rb" # don't let this file get moved
+
+    log_block "language server" do
+        info "install ruby rls: solargraph"
+        system "sudo gem install solargraph"
+    end
 end
 
 def uninstall
     info "remove my ruby scripts"
     system "rm -r ~/.config/mybin/brlyman"
     system "rm -r ~/.config/mybin/onChange.rb"
+
+    log_block "language servers" do
+        info "install ruby rls: solargraph"
+        system "sudo gem uninstall solargraph"
+    end
 end
 
 if ARGV.first == "-u"
